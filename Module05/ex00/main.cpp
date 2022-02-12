afccf9ef878613f6730d0443df3379389af879db
Omar Idrissi yazami <oidrissi@e2r3p3.1337.ma>
2022-02-12 15:25:00 +0100

    f

--- Module05/ex00/main.cpp
+++ Module05/ex00/main.cpp
@@ -3,45 +3,38 @@
 int main()
 {
     Bureaucrat b("Bob", 1);
-    Bureaucrat b2("Bobi", 151);
-    Bureaucrat b3("Mobi", 150);
+    Bureaucrat b2("Abby", 151);
+    Bureaucrat b3("Mordor", 150);
+    std::cout << "----------------  " << std::endl;
     std::cout << b << std::endl;
     std::cout << b2 << std::endl;
     std::cout << b3 << std::endl;
     
     try
     {
+        std::cout << "----------------  " << std::endl;
+        std::cout << "Incrementing grade of " << b.getName() << ", what happens?" << std::endl;
         b.incrementGrade();
-        std::cout << "Incrementing grade, what happens?" << std::endl;
     }
     catch (std::exception & e)
     {
         std::cerr << "Exception Caught: " << e.what() << std::endl;
+        std::cout << "----------------  " << std::endl;
     }
-    std::cout << b.getName() << " : "<< b.getGrade() << std::endl; 
-    // try
-    // {
-    //     b.incrementGrade();
-    // }
-    // catch (std::exception & e)
-    // {
-    //     std::cerr << "Exception Caught:" << e.what() << std::endl;
-    // }
-    // try
-    // {
-    //     b2.decrementGrade();
-    // }
-    // catch (std::exception & e)
-    // {
-    //     std::cout << e.what() << std::endl;
-    // }
-    // try
-    // {
-    //     b3.decrementGrade();
-    // }
-    // catch (std::exception & e)
-    // {
-    //     std::cout << e.what() << std::endl;
-    // }
+    try
+    {
+        std::cout << "Decrementing grade of " << b2.getName() << ", what happens?" << std::endl;
+        b2.decrementGrade();
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << "Exception Caught: " << e.what() << std::endl;
+        std::cout << "----------------  " << std::endl;
+
+    }
+    std::cout << b.getName() << " : "<< b.getGrade() << std::endl;
+    std::cout << b2.getName() << " : " << b2.getGrade() << std::endl;
+    std::cout << "----------------  " << std::endl;
+
     return 0;
 }

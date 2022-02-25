cc8103fd30c0b07b7b56728cb43b61899c17cda4
Omar Idrissi yazami <oidrissi@e1r13p11.1337.ma>
2022-02-25 20:57:11 +0100

    Mod

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: oidrissi <oidrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 02:36:33 by oidrissi          #+#    #+#             */
-/*   Updated: 2022/02/25 05:13:32 by oidrissi         ###   ########.fr       */
+/*   Updated: 2022/02/25 18:35:07 by oidrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ template <typename T>
 int		easyfind(T &container, int value)
 {
     typename T::iterator it;
-
+    // find is an STL algorithm that returns an iterator to the first element in the container that satisfies the predicate or condition.
     it = std::find(container.begin(), container.end(), value);
     if (it != container.end())
         return (*it);

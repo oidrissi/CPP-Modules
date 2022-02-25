f735a5105c25b2c5dfc1602baf27d261eeec99b7
Omar Idrissi yazami <oidrissi@e1r10p2.1337.ma>
2022-02-25 05:15:37 +0100

    Mod8

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -6,13 +6,29 @@
 /*   By: oidrissi <oidrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 02:36:33 by oidrissi          #+#    #+#             */
-/*   Updated: 2022/02/25 02:37:02 by oidrissi         ###   ########.fr       */
+/*   Updated: 2022/02/25 05:13:32 by oidrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+#include <iostream>
+#include <exception>
+#include <vector>
 
+
+template <typename T>
+int		easyfind(T &container, int value)
+{
+    typename T::iterator it;
+
+    it = std::find(container.begin(), container.end(), value);
+    if (it != container.end())
+        return (*it);
+    else
+        throw std::exception();
+}
 
 #endif
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -6,8 +6,35 @@
 /*   By: oidrissi <oidrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 02:37:05 by oidrissi          #+#    #+#             */
-/*   Updated: 2022/02/25 02:37:10 by oidrissi         ###   ########.fr       */
+/*   Updated: 2022/02/25 05:14:39 by oidrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+
+int main()
+{
+    int a[5] = {1, 2, 3, 4, 5};
+    std::vector<int> b(a, a + 5);
+    std::cout << "b: ";
+    for (std::vector<int>::iterator it = b.begin(); it != b.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+    try
+    {
+        std::cout << "easyfind(b, 3): " << easyfind(b, 3) << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    try
+    {
+        std::cout << "easyfind(b, 6): " << easyfind(b, 6) << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
+}
